4fd621472bd72354d6ae0285df5668186c1f2d91
Fernando Saccon <fernando@gssenergy.ca>
2017-10-11 10:02:46 -0600

    Initial commit

---
+++ BOL/Admins.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOL
+{
+    public class Admins
+    {
+        public int Id { get; set; }
+        public string IdentityId { get; set; }
+        public AppUser Identity { get; set; }  // navigation property
+        public string Location { get; set; }
+    }
+}
---
+++ Database/CodingBlastDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BOL;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class CodingBlastDbContext: IdentityDbContext<AppUser>
+    {
+        public CodingBlastDbContext(DbContextOptions<CodingBlastDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Admins> Admins { get; set; }
+    }
+}
